24d85d92d9ebf3d55c1e383460c025aa3bdb2f79
Noriko <fujine@sria.co.jp>
2020-08-27 18:42:01 +0900

    L字形に描画

--- main.js
+++ main.js
@@ -39,3 +39,29 @@ ctxTriangle.fill();
 canvasTriangle.appendChild(csTriangle);
 
 console.log(ctxTriangle);
+
+/************
+    多角形
+ ************/
+// 多角形を描画するエリア
+const canvasPolygon = document.getElementById("canvasPolygon");
+
+// Canvas要素取得
+const csPolygon = document.createElement("canvas");
+const ctxPolygon = csPolygon.getContext("2d");
+
+ctxPolygon.fillStyle = "rgba(0, 255, 0, .5)";
+ctxPolygon.beginPath();
+ctxPolygon.moveTo(0, 0);
+ctxPolygon.lineTo(0, 150);
+ctxPolygon.lineTo(300, 150);
+ctxPolygon.lineTo(300, 100);
+ctxPolygon.lineTo(50, 100);
+ctxPolygon.lineTo(50, 0);
+ctxPolygon.closePath();
+ctxPolygon.fill();
+
+// HTMLに作成した正方形追加
+canvasPolygon.appendChild(csPolygon);
+
+console.log(ctxPolygon);
