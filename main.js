5917dcca643997cb4e7e3ad244ef84aba41e0a24
Noriko <fujine@sria.co.jp>
2020-08-27 17:41:16 +0900

    試しに正方形作成

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 "use strict";
+const canvasArea = document.getElementById("canvasArea");
+const cs = document.createElement("canvas");
+const ctx = cs.getContext("2d");
+
+cs.width = "600";
+cs.height = "400";
+
+ctx.strokeRect(0, 0, 100, 100);
+ctx.fillRect(150, 0, 100, 100);
+
+canvasArea.appendChild(cs);
+
+console.log(cs);
+console.log(ctx);
